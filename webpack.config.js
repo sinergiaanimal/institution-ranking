cc1a8037b047d131d86f3b94f780f0225d7954ba
dzejkob <jakubstanclik@gmail.com>
2021-06-23 15:25:27 +0200

    Better style handling. Fixed problems with preview.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
 
 module.exports = {
   entry: './assets/index.js',  // path to our input file
   output: {
-    filename: 'index-bundle.js',  // output bundle file name
+    filename: 'bundle.js',  // output bundle file name
     path: path.resolve(__dirname, './dist'),  // path to our Django static directory
   },
 
   module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+
+    rules: [{
+      test: /\.s?css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ]
+    }],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+
+  optimization: {
+    minimize: true,  // to minimize also for development
+    minimizer: [
+      new CssMinimizerPlugin(),
     ],
   },
+
 };
