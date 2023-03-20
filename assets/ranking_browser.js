dfa5ae879b230fadc043d83c66e2cd139f75d4fa
dzejkobi <jakubstanclik@gmail.com>
2023-03-20 11:32:00 +0100

    Upgrade of vue-debounce package

--- assets/ranking_browser.js
+++ assets/ranking_browser.js
@@ -2,7 +2,7 @@ import 'bootstrap/js/dist/modal';
 
 import { createApp } from 'vue';
 import RankingBrowserApp from './vue/RankingBrowserApp.vue';
-import vueDebounce from 'vue-debounce';
+import { vue3Debounce } from 'vue-debounce';
 
 
 window.onload = function () {
@@ -13,6 +13,8 @@ window.onload = function () {
     );
     const cfgData = JSON.parse(cfgElem.innerHTML);
 
-    createApp(RankingBrowserApp, { cfg: cfgData }).use(vueDebounce).mount(elem);
+    createApp(RankingBrowserApp, { cfg: cfgData }).directive(
+      'debounce', vue3Debounce()
+    ).mount(elem);
   }
 };
